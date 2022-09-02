9215bb7c469182fb540e9aab06f274d7b05c4737
ruslan <sonikx94@yandex.ru>
2022-09-02 16:39:03 +0300

    Добавил секунды в формат времи в сообщение

--- internal/entity/message.go
+++ internal/entity/message.go
@@ -2,7 +2,7 @@ package entity
 
 import "time"
 
-const MessageTimeFormat = "2006-01-02 15:04"
+const MessageTimeFormat = "2006-01-02 15:04:05"
 
 type Message struct {
 	Id       int       `db:"id"`
